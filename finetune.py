40ba9bae58d59a1d9af422d4f9c530b8f394c1de
koala1618 <limm1618@gmail.com>
2025-05-07 18:59:14 +0800

    custom tokenizer perform poorly, change to pretrained bart tokenizer, increasing training epoch

--- finetune.py
+++ finetune.py
@@ -1,17 +1,11 @@
-from transformers import PreTrainedTokenizerFast
+from transformers import PreTrainedTokenizerFast, BartTokenizer
 from transformers import VisionEncoderDecoderModel, Seq2SeqTrainer, Seq2SeqTrainingArguments
 from data import load_latex_dataset, get_image_transform, HuggingfaceLatexDataset
 from datasets import concatenate_datasets
 
 
 # Tokenizer (BPE Tokenizer)
-tokenizer = PreTrainedTokenizerFast(
-    tokenizer_file="latex_tokenizer.json",  # path to your tokenizer
-    unk_token="<unk>",
-    pad_token="<pad>",
-    eos_token="</s>",
-    bos_token="<s>"
-)
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
 
 # input images preprocessing
 image_transform = get_image_transform()
@@ -31,26 +25,31 @@ model = VisionEncoderDecoderModel.from_encoder_decoder_pretrained(
 )
 
 # model configurations
-model.decoder.resize_token_embeddings(len(tokenizer))
 model.config.decoder_start_token_id = tokenizer.bos_token_id
 model.config.pad_token_id = tokenizer.pad_token_id
 model.config.eos_token_id = tokenizer.eos_token_id
 model.config.vocab_size = len(tokenizer)
 model.config.max_length = 600
 
+# print("Model Config:")
+# print(model.config.decoder_start_token_id)
+# print(model.config.eos_token_id)
+# print(model.config.pad_token_id)
+
 # trainig arguments
 training_args = Seq2SeqTrainingArguments(
-    output_dir="./finetuned-vit-bart",
-    learning_rate=5e-5,
-    per_device_train_batch_size=8,
-    num_train_epochs=5,
+    output_dir="./finetuned-vit-bart/version-3",
+    learning_rate=3e-5,
+    per_device_train_batch_size=16,
+    num_train_epochs=10,
+    weight_decay=0.01,
     logging_dir='./logs',
     fp16=True,
     save_strategy="epoch",
     eval_strategy="no",
     predict_with_generate=True,
     report_to="none",
-)   
+)    
 
 trainer = Seq2SeqTrainer(
     model = model,
